ca30c2f4088987479fa3079e3d94fd940f3b4508
Deity <69667434+TDeity@users.noreply.github.com>
2020-08-14 18:16:23 +0600

    Update eval.py

--- eval.py
+++ eval.py
@@ -16,9 +16,7 @@ import random
 
 """Evaluate Python commands directly from discord. Please note this command can be very dangerous so to protect
 your computer and your discord server we've limited this command to the bot owner only!
-
 Credits goes to Deity#6969, for any queries please contact me on Discord!
-
 REQUIREMENTS:
 discord.py 1.3.4 or higher
 """
@@ -221,15 +219,15 @@ class eval(commands.Cog):
             await ctx.message.add_reaction('\u2705')  
          
  
-@_eval.error
-async def _eval_error(self, ctx, error):
-    if isinstance(error, commands.CheckFailure):
-        await ctx.message.add_reaction('\u274c')  
-        await ctx.send(f"{ctx.author.mention} \u274c Your eval job has completed with return code 1.\n\n```Py\ndiscord.ext.commands.errors.{error.__class__.__name__}: {error}\n```")
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.message.add_reaction('\u274c') 
-        await ctx.send(f"{ctx.author.mention} \u274c Your eval job has completed with return code 1.\n\n```Py\ndiscord.ext.commands.errors.{error.__class__.__name__}: {error}\n```")
-            
+    @_eval.error
+    async def _eval_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.message.add_reaction('\u274c')  
+            await ctx.send(f"{ctx.author.mention} \u274c Your eval job has completed with return code 1.\n\n```Py\ndiscord.ext.commands.errors.{error.__class__.__name__}: {error}\n```")
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.message.add_reaction('\u274c') 
+            await ctx.send(f"{ctx.author.mention} \u274c Your eval job has completed with return code 1.\n\n```Py\ndiscord.ext.commands.errors.{error.__class__.__name__}: {error}\n```")
+                
             
 
 def setup(client):
