25ea96aeb9fc35dc3e6a03800a664ae6ff99148c
Deity <69667434+TDeity@users.noreply.github.com>
2020-08-14 16:51:18 +0600

    Update eval.py

--- eval.py
+++ eval.py
@@ -14,7 +14,7 @@ import time
 import random
 
 
-"""Evaluate Python commands directly through discord. Please note this command can be very dangerous so to protect
+"""Evaluate Python commands directly from discord. Please note this command can be very dangerous so to protect
 your computer and your discord server we've limited this command to the bot owner only!
 
 Credits goes to Deity#6969, for any queries please contact me on Discord!
